db26f02a09050d7a49c85fdb30dd1ab71047adc8
Muhammed Eren Cennetkuşu <programcieren@hotmail.com>
2023-11-19 22:12:53 +0300

    Update Model.php

--- php-smfony-routing-admin-panel/app/Models/Model.php
+++ php-smfony-routing-admin-panel/app/Models/Model.php
@@ -451,52 +451,6 @@ class Model   {
 
 
 
-    function MailGonderData( $posta_mail, $posta_konu, $posta_mesaj,$serverData ) {
-
-        $mail = new PHPMailer();
-        try {
-            //Sunucu ayarları
-            //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-            // Hata ayıklamak için debug etkin
-            $mail->isSMTP();
-            // SMTP kullanarak gönderim
-            $mail->Host       = 'smtp.gmail.com';
-            // SMTP sunucusu
-            $mail->SMTPAuth   = true;
-            // SMTP kimlik doğrulaması etkin
-            $mail->Username   = 'cennetkusueren@gmail.com';
-            // SMTP kullanıcısı
-            $mail->Password   = 'lvggcguzjlckdsjo';
-            // SMTP şifre
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-            // TLS ile şifreleme etkin
-            $mail->Port       = 587;
-            // SMTP port
-            //Karakter ayarları
-            $mail->CharSet  = 'utf-8';
-            //Türkçe karakter sorununun önüne geçecektir.
-            $mail->Encoding = 'base64';
-            // Alıcılar
-            $mail->setFrom( $posta_mail, $posta_konu );
-            $mail->addAddress( $posta_mail, $posta_konu );
-            $mail->AddAttachment($serverData);
-            // Alıcı
-            //$mail->addReplyTo( 'info@ornek.com', 'Bilgi' );
-            //$mail->addCC( 'cc@ornek.com' );
-            //$mail->addBCC( 'bcc@ornek.com' );
-            // İçerik
-            $mail->isHTML( true );
-            // Mail HTML formatında olacaktır.
-            $mail->Subject = $posta_konu;
-            $mail->Body    = $posta_mesaj;
-            $mail->AltBody = 'non-HTML mail istemcileri için mesaj gövdesidir.';
-            $mail->send();
-
-        } catch ( Exception $e ) {
-
-        }
-
-    }
 
 
 
